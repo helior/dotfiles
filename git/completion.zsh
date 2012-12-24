77121b0bf2d23fe4e63b38071f5051d7e2b174ae
Helior Colorado <me@helior.info>
2012-12-24 02:47:44 -0800

    Added git/completion.zsh

---
+++ git/completion.zsh
@@ -0,0 +1,12 @@
+# Uses git's autocompletion for inner commands. Assumes an install of git's
+# bash `git-completion` script at $completion below (this is where Homebrew
+# tosses it, at least).
+completion=( '/usr/local/etc/bash_completion.d/git-completion.bash' '/usr/local/etc/bash_completion.d/git-prompt.sh' '/usr/local/etc/bash_completion.d/hub.bash_completion.sh');
+
+for file in "${completion[@]}"
+do
+  if test -f $file
+  then
+  source $file
+  fi
+done
