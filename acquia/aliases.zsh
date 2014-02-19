b32e3194e355ceaee8efcac507799bfe924f0870
Helior Colorado <me@helior.info>
2014-02-19 13:34:24 -0800

    Added Acquia-specific aliases.

---
+++ acquia/aliases.zsh
@@ -0,0 +1,4 @@
+alias amysql='/Applications/Dev\ Desktop/mysql/bin/mysql -u root'
+alias amysqldump='/Applications/Dev\ Desktop/mysql/bin/mysqldump -u root'
+alias aphp='/Applications/Dev\ Desktop/php5_4/bin/php'
+alias aconfig='subl /Applications/Dev\ Desktop/apache/conf/httpd.conf /Applications/Dev\ Desktop/apache/conf/vhosts.conf /Applications/Dev\ Desktop/php5_4/bin/php.ini'
