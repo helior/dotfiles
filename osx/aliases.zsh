690a5ff226d52ac0af9b226cb19dd136b2eef88e
Helior Colorado <me@helior.info>
2014-03-04 13:29:33 -0800

    Helpful comments.

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -4,8 +4,8 @@ alias ql='qlmanage -p'
 # Open current directory in Finder
 alias oo="open ."
 
-# Disable ethernet network adaptor
+# Disable ethernet network adaptor, use `networksetup -listallhardwareports` to view devices available
 alias ethoff="sudo ifconfig en0 down"
 
-# Enable ethernet network adaptor
+# Enable ethernet network adaptor, use `networksetup -listallhardwareports` to view devices available
 alias ethon="sudo ifconfig en0 up"
