977f1081af4031eec6b1d94d44c5be65e075c892
Helior Colorado <me@helior.info>
2014-03-04 13:16:10 -0800

    Aliases to enable/disable ethernet network device.

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -3,3 +3,9 @@ alias ql='qlmanage -p'
 
 # Open current directory in Finder
 alias oo="open ."
+
+# Disable ethernet network adaptor
+alias ethoff="sudo ifconfig en0 down"
+
+# Enable ethernet network adaptor
+alias ethon="sudo ifconfig en0 up"
