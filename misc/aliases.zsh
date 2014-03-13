aada525f70371259f08d7af43c9243fd1e171605
Helior Colorado <me@helior.info>
2014-03-13 15:48:12 -0700

    Easy-peasy git alias to fetch pull-requests from upstream

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -10,3 +10,6 @@ function tabname {
 function winname {
   printf "\e]2;$1\a"
 }
+
+# TODO: find a place for this. Get Akamai headers
+alias akcurlheaders='curl -H "Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-get-request-id" -v -o /dev/null'
