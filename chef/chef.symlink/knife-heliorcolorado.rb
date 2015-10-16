9ba492b864d74f843c29546f3fd2dadfff729739
Helior Colorado <me@helior.info>
2015-10-16 11:42:56 -0700

    Add heliorcolorado organization knife config

---
+++ chef/chef.symlink/knife-heliorcolorado.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "heliorcolorado"
+client_key               "#{current_dir}/heliorcolorado.pem"
+validation_client_name   "heliorcolorado-validator"
+validation_key           "#{current_dir}/heliorcolorado-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/heliorcolorado"
+cookbook_path            ["#{current_dir}/../cookbooks"]
