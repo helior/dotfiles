5f538da914052f043c1c328cca4c1102e0033911
Helior Colorado <me@helior.info>
2015-12-22 17:43:07 -0800

    Move lookitsunienvs knife pem files

--- chef/chef.symlink/knife-lookitsunienvs.rb
+++ chef/chef.symlink/knife-lookitsunienvs.rb
@@ -4,8 +4,11 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "heliorcolorado"
-client_key               "#{current_dir}/heliorcolorado.pem"
+client_key               "#{current_dir}/data/lookitsunienvs/heliorcolorado.pem"
 validation_client_name   "lookitsunienvs-validator"
-validation_key           "#{current_dir}/lookitsunienvs-validator.pem"
+validation_key           "#{current_dir}/data/lookitsunienvs/lookitsunienvs-validator.pem"
 chef_server_url          "https://api.chef.io/organizations/lookitsunienvs"
-cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            ["#{current_dir}/data/lookitsunienvs/cookbooks"]
+data_bag_path            ["#{current_dir}/data/lookitsunienvs/data_bags"]
+
+knife[:vault_mode] = 'client'
