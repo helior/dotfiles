925ecaf44660ced9c1a8acfb402b10a9babee5f9
Helior Colorado <me@helior.info>
2017-05-05 10:38:41 -0700

    yarn; um/ud; chef symlink

--- Brewfile
+++ Brewfile
@@ -24,6 +24,7 @@ install composer
 install drush
 install rbenv
 install ruby-build
+install yarn
 
 # Devops Tooling
 tap homebrew/binary
--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,3 +2,5 @@ alias git=hub
 alias g="git"
 
 alias todo="git grep -l TODO | xargs -n1 git blame -f | grep TODO"
+alias um="git pull upstream master"
+alias ud="git pull upstream dev"
