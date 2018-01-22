42430639d7b4780521d84f83f4216fd7ae85ac68
Helior Colorado <me@helior.info>
2018-01-22 12:47:53 -0800

    Add xcode alias to open a project.

--- xcode/alias.zsh
+++ xcode/alias.zsh
@@ -1,1 +1,2 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/iOS\ Simulator.app"
+alias xcode="open -a Xcode *.xcworkspace 2>/dev/null || open -a Xcode *.xcodeproj 2>/dev/null || echo \"Ughhhhhh.... there's nothing here to open.\" && false"
