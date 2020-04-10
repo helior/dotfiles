681032d13641900cf9bd74467a9611114e493e8b
Helior Colorado <me@helior.info>
2020-04-10 00:03:34 -0700

    Unknown docker util being added?

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,3 +1,6 @@
 docker-ip() {
   docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
 }
+
+# Homebrew: dvm is a shell function, and must be sourced before it can be used.
+[ -f /usr/local/opt/dvm/dvm.sh ] && . /usr/local/opt/dvm/dvm.sh
