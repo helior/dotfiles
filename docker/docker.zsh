36e8c6278f8eaa1b52ebc6a8633da89f2d00bd7a
Helior Colorado <me@helior.info>
2020-04-10 15:49:36 -0700

    Handy docker aliases

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,3 +1,7 @@
 docker-ip() {
   docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
 }
+
+alias dcl="docker container ls --all"
+alias dcr="docker container rm -f"
+alias dip="docker rmi $(docker images -f "dangling=true" -q)"
