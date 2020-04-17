0223f191d4d31b130ce497a988ff47e1944bcac8
Helior Colorado <me@helior.info>
2020-04-17 01:37:12 -0700

    Python + Pip + VirtualEnv

--- python/python.zsh
+++ python/python.zsh
@@ -1,7 +1,18 @@
+# Install Steps + tools:
+# > brew install python
+# > curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# > python get-pip.py
+# > pip --version
+# > pip list -v
+# > pip install virtualenv
+# > virtualenv ${environment_name}
+
+
 ### Add Go binaries to PATH
 export PATH="$HOME/Library/Python/3.7/bin:$PATH"
 export PATH="$HOME/Library/Python/2.7/bin:$PATH"
 export PATH="$HOME/Library/Python/3.6/bin:$PATH"
+export PATH="/usr/local/lib/python3.7/site-packages:$PATH"
 
 ## This is my Pro solution to switch Python  versions.
-alias python="python3"
+# alias python="python3"
