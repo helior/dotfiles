791cdccb7a3997e3e9098983885e8043f0de19a1
Helior Colorado <me@helior.info>
2020-04-26 13:31:13 -0700

    Added alias deload! to edit dotfiles

--- helior/helior.zsh
+++ helior/helior.zsh
@@ -1,3 +1,5 @@
+alias reload!='source ~/.zshrc'
+
 alias rrr="reload!"
 alias rrrr="reload!"
 alias rrrrr="reload!"
@@ -6,3 +8,14 @@ alias rrrrrrr="reload!"
 alias rrrrrrrr="reload!"
 alias rrrrrrrrr="reload!"
 alias rrrrrrrrrr="reload!"
+
+alias deload!='cd ~/.dotfiles && code .'
+
+alias ddd="deload!"
+alias dddd="deload!"
+alias ddddd="deload!"
+alias dddddd="deload!"
+alias ddddddd="deload!"
+alias dddddddd="deload!"
+alias ddddddddd="deload!"
+alias dddddddddd="deload!"
