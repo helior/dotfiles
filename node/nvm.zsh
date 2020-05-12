1f4d0a54854737ad78f874d10b7231b49fa4162a
Helior Colorado <me@helior.info>
2020-05-11 22:11:09 -0700

    This slows down terminal window startups

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,6 +1,6 @@
 ### Added by NVM installer (originally, anyway)
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 ### Auto-detect .nvmrc
 # place this after nvm initialization!
