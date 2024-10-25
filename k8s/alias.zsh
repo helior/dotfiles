8c537a8f77381acbe5ac3010145ee6247efe520d
Helior Colorado <me@helior.info>
2024-10-24 23:16:28 -0700

    ⭐️ kc: kubectl config; kx: exec pod

--- k8s/alias.zsh
+++ k8s/alias.zsh
@@ -1,2 +1,10 @@
 alias k=kubectl
 alias ka=kubeadm
+alias kc="kubectl config"
+kx() {
+  if [ -z "$1" ]; then
+    echo "Usage: kexec <pod-name>"
+    return 1
+  fi
+  kubectl exec -it "$1" -- "/bin/sh"
+}
